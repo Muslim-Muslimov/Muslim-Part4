c486c57eaee995382487d807d8eda97539a2f880
Муслим Марашев <appl95@bk.ru>
2023-11-30 19:46:11 +0300

    сделал 2 задачу

---
+++ Muslim-Part4/ReviewAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muslim_Part4
+{
+    internal class ReviewAnalyzer
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Оставьте отзыв: ");
+            string отзыв = Console.ReadLine();
+
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Muslim_Part4;
+
+ReviewAnalyzer.Start();
